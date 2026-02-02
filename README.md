4b2204769c4360ec368a7d56a670ceceb2b76811
Joshua Banfield <joshuabanfield21@gmail.com>
2026-02-02 20:55:50 +0000

    Updated README.md.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [DEMO](https://gsap-milkshakes-mocha.vercel.app)
 
 Following a tutorial from: [Fullstack Empire](https://www.youtube.com/watch?v=pqYxZ8jd768)
+
+Design and assets are the property of [SPYLT Milk](https://spylt.com)
--- src/components/FlavourTitle.jsx
+++ src/components/FlavourTitle.jsx
@@ -29,28 +29,38 @@ const FlavourTitle = () => {
         trigger: ".flavor-section",
         start: "top 10%",
       }
-    })
+    });
+
+    gsap.from(secondTextSplit.chars, {
+      yPercent: 200,
+      stagger: 0.02,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "top 1%",
+      },
+    });
   });
 
   return (
     <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
-        <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
-            <h1>
-                We have 6
-            </h1>
-        </div>
+      <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
+        <h1>
+          We have 6
+        </h1>
+      </div>
 
-        <div style={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)" }} className="flavor-text-scroll">
-          <div className="bg-mid-brown pb-6 2xl:pt-0 pt-3 2xl:px-5 px-3">
-            <h2 className="text-milk">Freaking</h2>
-          </div>
+      <div style={{ clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)" }} className="flavor-text-scroll">
+        <div className="bg-mid-brown pb-3 2xl:pt-0 pt-3 2xl:px-5 px-3">
+          <h2 className="text-milk">Freaking</h2>
         </div>
+      </div>
 
-        <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
-          <h1>
-            Delicious flavours
-          </h1>
-        </div>
+      <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
+        <h1>
+          Delicious flavours
+        </h1>
+      </div>
     </div>
   )
 }
